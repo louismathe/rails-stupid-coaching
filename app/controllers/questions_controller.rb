6c693fe9d71f94cee836fd188f0faeb4f0cc5cb7
Louis Mathe <mathe.louis@gmail.com>
2017-11-06 14:05:32 +0100

    rails new stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def answer
+    @query = params[:query]
+
+    if @query.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @query == "I am going to work right now!"
+      @answer = "Good job buddy!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+
+  end
+  def ask
+  end
+
+end
